ff97b624d6e4ca7f83e237db32f77bdddee1b7fb
milk717 <robolindasoo@gmail.com>
2026-02-03 14:47:54 +0900

    chore: Bump version to 1.1
    Update the application version in `app/build.gradle.kts`:
    - Set `versionCode` to 2.
    - Set `versionName` to "1.1".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "me.milk717.rubatomanager"
         minSdk = 36
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
